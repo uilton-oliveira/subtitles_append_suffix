ed1331e86d18c60f8b2042e7c6d5272531d4fbdf
Uilton Oliveira <uilton.dev@gmail.com>
2023-05-27 21:48:38 -0300

    proper handling .forced subtitles

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.8-alpine
+FROM python:3-alpine
 
 # Setup env
 ENV LANG C.UTF-8
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 
 def rename():
-    paths = os.getenv('SUBTITLE_RENAME_DIR', '/rename').lower()
+    paths = os.getenv('SUBTITLE_RENAME_DIR', './test')
     suffix = os.getenv('SUBTITLE_SUFFIX', '.pt').lower()
     ext = os.getenv('SUBTITLE_EXT', '.srt').lower()
 
@@ -12,12 +12,16 @@ def rename():
         for r, d, f in os.walk(path):
             for file in f:
                 file_ext = os.path.splitext(file)
-                if file_ext[1].lower() == ext and not file_ext[0].lower().endswith(suffix):
+                name = file_ext[0].lower()
+                if file_ext[1].lower() == ext and not (name.endswith(suffix) or name.endswith(f'{suffix}.forced')):
                     files.append(os.path.join(r, file))
 
         for f in files:
             file_ext = os.path.splitext(f)
-            new_name = f'{file_ext[0]}{suffix}{file_ext[1]}'
+            if file_ext[0].endswith('.forced'):
+                new_name = f'{file_ext[0][:-7]}{suffix}.forced{file_ext[1]}'
+            else:
+                new_name = f'{file_ext[0]}{suffix}{file_ext[1]}'
             print(f'renamed: {f} -> {new_name}')
             os.rename(f, new_name)
 
